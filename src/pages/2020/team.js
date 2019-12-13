16f0d4fd898d29a0dd2bcc6cfbbb0e4e43cca521
williamars <williamars@al.insper.edu.br>
2019-12-12 21:22:01 -0300

    Atualizei algumas classes

--- src/pages/2020/team.js
+++ src/pages/2020/team.js
@@ -15,76 +15,76 @@ const EquipePage = () => (
       <div class="hero-body">
         <div class="container">
           <h1 class="title is-2 block-title-bw">Equipe 2020</h1>
-          <h2 class="titles-team">Administrativo</h2>
+          <h2 class="is-size-4 titles-team">Administrativo</h2>
           <div class="columns is-gapless">
             <div class="column">
               <img class="images-team" src={icon} alt="Ícone Mileage" />
-              <h1 class="names">Celine Mitu</h1>
-              <h2 class="office">Administrativo</h2>
+              <h1 class="is-size-5 names">Celine Mitu</h1>
+              <h2 class="is-size-6 office">Administrativo</h2>
             </div>
             <div class="column">
               <img class="images-team" src={icon} alt="Ícone Mileage" />
-              <h1 class="names">Igor Seramisu</h1>
-              <h2 class="office">Administrativo</h2>
+              <h1 class="is-size-5 names">Igor Seramisu</h1>
+              <h2 class="is-size-6 office">Administrativo</h2>
             </div>
             <div class="column">
               <img class="images-team" src={icon} alt="Ícone Mileage" />
-              <h1 class="names">João Victor</h1>
-              <h2 class="office">Administrativo</h2>
+              <h1 class="is-size-5 names">João Victor</h1>
+              <h2 class="is-size-6 office">Administrativo</h2>
             </div>
             <div class="column">
               <img class="images-team" src={icon} alt="Ícone Mileage" />
-              <h1 class="names">Ramiro Pandullo</h1>
-              <h2 class="office">Administrativo</h2>
+              <h1 class="is-size-5 names">Ramiro Pandullo</h1>
+              <h2 class="is-size-6 office">Administrativo</h2>
             </div>
           </div>
           <div class="columns is-gapless">
             <div class="column">
               <img class="images-team" src={icon} alt="Ícone Mileage" />
-              <h1 class="names">Martim Ferreira José</h1>
-              <h1 class="office">Líder de Software</h1>
+              <h1 class="is-size-5 names">Martim Ferreira José</h1>
+              <h1 class="is-size-6 office">Líder de Software</h1>
             </div>
             <div class="column">
               <img class="images-team" src={icon} alt="Ícone Mileage" />
-              <h1 class="names">Guilherme Roccato</h1>
-              <h1 class="office">Líder de Controle</h1>
+              <h1 class="is-size-5 names">Guilherme Roccato</h1>
+              <h1 class="is-size-6 office">Líder de Controle</h1>
             </div>
             <div class="column">
               <img class="images-team" src={icon} alt="Ícone Mileage" />
-              <h1 class="names">Felipe Scandiuzzi</h1>
-              <h1 class="office">Líder de Mecânica</h1>
+              <h1 class="is-size-5 names">Felipe Scandiuzzi</h1>
+              <h1 class="is-size-6 office">Líder de Mecânica</h1>
             </div>
             <div class="column">
               <img class="images-team" src={icon} alt="Ícone Mileage" />
-              <h1 class="names">Victor Hugo Leal</h1>
-              <h1 class="office">Líder de Eletrônica</h1>
+              <h1 class="is-size-5 names">Victor Hugo Leal</h1>
+              <h1 class="is-size-6 office">Líder de Eletrônica</h1>
             </div>
           </div>
           <div class="columns is-gapless">
             <div class="column">
               <img class="images-team" src={icon} alt="Ícone Mileage" />
-              <h1 class="names">Lucas Fontenla</h1>
-              <h1 class="office">Capitão da Equipe</h1>
+              <h1 class="is-size-5 names">Lucas Fontenla</h1>
+              <h1 class="is-size-6 office">Capitão da Equipe</h1>
             </div>
           </div>
-          <h1 class="titles-team">Departamentos</h1>
+          <h1 class="is-size-4 titles-team">Departamentos</h1>
           <div class="has-horizontal-scroll has-text-centered">
             <div class="scrolling-wrapper-flexbox">
-
               <div class="departaments">
                 <figure class="images-team">
                   <img src={SoftControlIcon} alt="Software" />
                 </figure>
-                <h5 class="names">Software</h5>
-                <h6 class="descriptions">A área de software é responsável por fazer isso aquilo e mais isso.</h6>
+                <h5 class="is-size-4 names">Software</h5>
+                <h6 class="is-size-5 descriptions">Área responsável por estabelecer o meio de comunicação dos dados com o piloto, para que possa auxiliá-lo na pista. Ademais, 
+                também mantenedor </h6>
               </div>
 
               <div class="departaments">
                 <figure class="images-team">
                   <img src={SoftControlIcon} alt="Controle" />
                 </figure>
-                <h5 class="names">Controle</h5>
-                <h6 class="descriptions">A área de controle é responsável por fazer isso aquilo e mais isso.</h6>
+                <h5 class="is-size-4 names">Controle</h5>
+                <h6 class="is-size-5 descriptions">A área de controle é responsável por fazer isso aquilo e mais isso.</h6>
               </div>
               <div class="padder-card " />
             </div>
@@ -96,16 +96,16 @@ const EquipePage = () => (
                 <figure class="images-team">
                   <img src={MechanicIcon} alt="Mecânica" />
                 </figure>
-                <h5 class="names">Mecânica</h5>
-                <h6 class="descriptions">A área de mecânica é responsável por fazer isso aquilo e mais isso.</h6>
+                <h5 class="is-size-4 names">Mecânica</h5>
+                <h6 class="is-size-5 descriptions">A área de mecânica é responsável por fazer isso aquilo e mais isso.</h6>
               </div>
 
               <div class="departaments">
                 <figure class="images-team">
                   <img src={EletronicIcon} alt="Eletrônica" />
                 </figure>
-                <h5 class="names">Eletrônica</h5>
-                <h6 class="descriptions">A área de eletrônica é responsável por fazer isso aquilo e mais isso.</h6>
+                <h5 class="is-size-4 names">Eletrônica</h5>
+                <h6 class="is-size-5 descriptions">A área de eletrônica é responsável por fazer isso aquilo e mais isso.</h6>
               </div>
               <div class="padder-card " />
             </div>
