5f4e4fceb11dc17f8fb3e50898dacdf865e122e3
nicole costa <nicolesarvasi@hotmail.com>
2020-07-13 10:59:33 -0300

    prototype component

--- src/pages/2020/project.js
+++ src/pages/2020/project.js
@@ -5,8 +5,8 @@ import AnchorLink from "react-anchor-link-smooth-scroll"
 import { OutboundLink } from "gatsby-plugin-google-analytics"
 
 import SEO from "../../components/SEO"
-import PrototypeComponent from "../components/PrototypeComponent"
-import Prototype2Component from "../components/Prototype2Component"
+import PrototypeComponent from "../../components/PrototypeComponent"
+import Prototype2Component from "../../components/Prototype2Component"
 
 import prototype_sketch from "../../images/2020/project/prototype-sketch.png"
 import prototype1 from "../../images/2020/project/prototype-1.png"
@@ -105,61 +105,7 @@ const Project = () => (
                                                                     info = "30 kg" />
                                                 <PrototypeComponent titulo = "Telemetria"
                                                                     info = "Em tempo real com dashboard na nuvem" />
-                                            
-                                                <tr>
-                                                    <th>Astronomia</th>
-                                                    <td>__ km</td>
-                                                </tr>
-                                                <tr>
-                                                    <th>Consumo</th>
-                                                    <td>170km/kWh</td>
-                                                </tr>
-                                                <tr>
-                                                    <th>Bateria</th>
-                                                    <td>24V 5000mAh</td>
-                                                </tr>
-                                                <tr>
-                                                    <th>Motor</th>
-                                                    <td>Brushless 24V 350W</td>
-                                                </tr>
-                                                <tr>
-                                                    <th>Sistema de direção</th>
-                                                    <td>
-                                                        Geometria de Ackerman
-                                                        com Pitman Arm
-                                                    </td>
-                                                </tr>
-                                                <tr>
-                                                    <th>Transmissão</th>
-                                                    <td>
-                                                        18:1, fixa e por
-                                                        engrenagens
-                                                    </td>
-                                                </tr>
-                                                <tr>
-                                                    <th>Chassi</th>
-                                                    <td>Tubular em alumínio</td>
-                                                </tr>
-                                                <tr>
-                                                    <th>Carenagem</th>
-                                                    <td>
-                                                        Fibra de carbono e
-                                                        policarbonato
-                                                    </td>
-                                                </tr>
-                                                <tr>
-                                                    <th>Peso</th>
-                                                    <td>30 kg</td>
-                                                </tr>
-                                                <tr>
-                                                    <th>Telemetria</th>
-                                                    <td>
-                                                        Em tempo real com
-                                                        dashboard na nuvem
-                                                    </td>
-                                                </tr>
-
-                                        
+        
                                             </tbody>
                                         </table>
                                     </article>
@@ -170,7 +116,8 @@ const Project = () => (
                 </div>
                 <Prototype2Component titulo = "Administrativo"
                                      info = "O departamento administrativo é responsável por toda gestão administrativa do Insper Mileage.
-                                     Seu papel comporta áreas essenciais para gestão de um projeto sendo elas:
+                                     Seu papel comporta áreas essenciais para gestão de um projeto sendo elas:                     
+                                
                                      • Finanças
                                      • Captação de recursos
                                      • Marketing
@@ -182,7 +129,6 @@ const Project = () => (
                 <Prototype2Component titulo = "Controle"
                                      info = "A área de controle do Insper Mileage é responsável pelo desenvolvimento da modelagem física do veículo, projeto da dinâmica veicular como dimensionamento de motores, baterias e estudo da aerodinâmica, assim como o controle de eficiência energética do carro, desempenho em frenagem, aceleração, curvas e consumo de energia. Sendo assim, é nosso papel determinar parâmetros ótimos para o carro ser o mais seguro, rápido e econômico possível, bem como prever o seu comportamento dinâmico antes do protótipo ser finalizado." />
 
-                                     A área de controle do Insper Mileage é responsável pelo desenvolvimento da modelagem física do veículo, projeto da dinâmica veicular como dimensionamento de motores, baterias e estudo da aerodinâmica, assim como o controle de eficiência energética do carro, desempenho em frenagem, aceleração, curvas e consumo de energia. Sendo assim, é nosso papel determinar parâmetros ótimos para o carro ser o mais seguro, rápido e econômico possível, bem como prever o seu comportamento dinâmico antes do protótipo ser finalizado.
                 <Prototype2Component titulo = "Eletrônica"
                                      info = "A área de eletrônica é responsável pela organização e produção de qualquer circuito elétrico e eletrônico do protótipo, podendo ser dividida em três setores principais, no caso, acionamento do motor, instrumentação e segurança. A parte de acionamento do motor, consiste no estudo e confecção de circuitos que permitem a alimentação do motor através da bateria e o controle de funcionamento do mesmo, como velocidade. A parte de instrumentação é resumida em circuitos de sensores capazes de captar variáveis importantes do carro para a análise dos dados. Por fim, a segurança consiste em toda e qualquer aplicação de precaução de incidentes com os componentes elétricos do carro." />
 
@@ -193,67 +139,6 @@ const Project = () => (
                                      info = "A área de software é responsável pelo sistema de telemetria do carro. Para isso, nos dividimos em três subáreas: backend, responsável por fazer o tratamento de dados e armazená-los; frontend, responsável por criar uma interface com o piloto e uma dashboard para a equipe, capaz de atualizar os dados em tempo real; e, por fim, a terceira subárea realiza uma análise de dados para garantir que o carro está tendo a melhor eficiência possível." />
 
 
-
-
-
-                <div class="container">
-                    <h3 id="administrativo" class="title is-4 block-title-bw ">
-                        Administrativo
-                    </h3>
-                    <div class="columns">
-                        <div class="column">
-                            O departamento administrativo é responsável por toda gestão administrativa do Insper Mileage.
-                            Seu papel comporta áreas essenciais para gestão de um projeto sendo elas:
-                            • Finanças
-                            • Captação de recursos
-                            • Marketing
-                            • Recursos humanos
-                            • Logística.
-
-                            Para isso busca diariamente gerir o pessoal e as atividades essências do projeto, alem de juntamente a diretoria estruturar o direcionamento estrategico.
-                            </div>
-                    </div>
-                </div>
-                <div class="container">
-                    <h3 id="controle" class="title is-4 block-title-bw ">
-                        Controle
-                    </h3>
-                    <div class="columns">
-                        <div class="column">
-                            A área de controle do Insper Mileage é responsável pelo desenvolvimento da modelagem física do veículo, projeto da dinâmica veicular como dimensionamento de motores, baterias e estudo da aerodinâmica, assim como o controle de eficiência energética do carro, desempenho em frenagem, aceleração, curvas e consumo de energia. Sendo assim, é nosso papel determinar parâmetros ótimos para o carro ser o mais seguro, rápido e econômico possível, bem como prever o seu comportamento dinâmico antes do protótipo ser finalizado.
-                            </div>
-                    </div>
-                </div>
-                <div class="container">
-                    <h3 id="eletronica" class="title is-4 block-title-bw">
-                        Eletrônica
-                    </h3>
-                    <div class="columns">
-                        <div class="column">
-                            A área de eletrônica é responsável pela organização e produção de qualquer circuito elétrico e eletrônico do protótipo, podendo ser dividida em três setores principais, no caso, acionamento do motor, instrumentação e segurança. A parte de acionamento do motor, consiste no estudo e confecção de circuitos que permitem a alimentação do motor através da bateria e o controle de funcionamento do mesmo, como velocidade. A parte de instrumentação é resumida em circuitos de sensores capazes de captar variáveis importantes do carro para a análise dos dados. Por fim, a segurança consiste em toda e qualquer aplicação de precaução de incidentes com os componentes elétricos do carro.
-                            </div>
-                    </div>
-                </div>
-                <div class="container">
-                    <h3 id="mecanica" class="title is-4 block-title-bw">
-                        Mecânica
-                    </h3>
-                    <div class="columns">
-                        <div class="column">
-                            Fazemos desde dimensionamentos e projeto dos componentes mecânicos do carro (chassis, sistemas de transmissão, direção e carenagem), seleção de componentes a serem comprados, além da fabricação própria de partes do protótipo (por usinagem, corte à plasma, laminação de compósitos, entre outros). A montagem do carro é realizada na própria oficina.
-                        </div>
-                    </div>
-                </div>
-                <div class="container">
-                    <h3 id="software" class="title is-4 block-title-bw">
-                        Software
-                    </h3>
-                    <div class="columns">
-                        <div class="column">
-                            A área de software é responsável pelo sistema de telemetria do carro. Para isso, nos dividimos em três subáreas: backend, responsável por fazer o tratamento de dados e armazená-los; frontend, responsável por criar uma interface com o piloto e uma dashboard para a equipe, capaz de atualizar os dados em tempo real; e, por fim, a terceira subárea realiza uma análise de dados para garantir que o carro está tendo a melhor eficiência possível.
-                        </div>
-                    </div>
-                </div>
             </div>
         </section >
     </Layout >
