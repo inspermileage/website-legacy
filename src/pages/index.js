b8d8d8c758d54812cda567c6a93fe908a822f101
Beatriz Rianho Bernadinho <pbeatriz18@hotmail.com.br>
2020-09-09 18:29:30 -0300

    mudando rotas

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,7 @@
 import React from "react"
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
-import InicialPage from "../initialPage"
-import Blog from "./blog"
 
-const IndexPage = () => (
-    <Router>
-        <main>
-            <Switch>
-                <Route exact path="/" component={InicialPage} />
-                <Route exact path="/blog" component={Blog} />
-            </Switch>
-        </main>
-    </Router>
-)
+import InicialPage from "../initialPage"
+
+const IndexPage = () => <InicialPage />
 
 export default IndexPage
