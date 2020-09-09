d21c0bc61e4c872527300ddf18a3ac27ca99a03d
Beatriz Rianho Bernadinho <pbeatriz18@hotmail.com.br>
2020-09-09 17:52:51 -0300

    erro de pull request

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -2,83 +2,76 @@ import React from "react"
 import { graphql } from "gatsby"
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 import Img from "gatsby-image"
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 import SEO from "../components/SEO"
 
 export const query = graphql`
-  query(
-    $slug: String!
-  ){
-    contentfulBlog(
-      slug:{
-        eq:$slug
-      }
-    ){
-      title
-      slug
-      date(formatString:"MMMM Do, YYYY")
-      description{
-        json
-      }
-      image{
-        file {
-          url
+    query($slug: String!) {
+        contentfulBlog(slug: { eq: $slug }) {
+            title
+            slug
+            date(formatString: "MMMM Do, YYYY")
+            description {
+                json
+            }
+            image {
+                file {
+                    url
+                }
+                title
+                fixed {
+                    base64
+                    tracedSVG
+                    aspectRatio
+                    srcWebp
+                    srcSetWebp
+                    height
+                    width
+                    src
+                    srcSet
+                }
+            }
         }
-        title
-        fixed {
-          base64
-          tracedSVG
-          aspectRatio
-          srcWebp
-          srcSetWebp
-          height
-          width
-          src
-          srcSet
-        }
-      }
     }
-  }
 `
 
-const Blog = props => {
-  const options = {
-    renderNode: {
-      "embedded-asset-block": (node) => {
-        const alt = node.data.target.fields.title["en-US"]
-        const url = node.data.target.fields.file["en-US"].url
-        return <img alt={alt} src={url} />
-      },
-    },
-  }
+const Blog = (props) => {
+    const options = {
+        renderNode: {
+            "embedded-asset-block": (node) => {
+                const alt = node.data.target.fields.title["en-US"]
+                const url = node.data.target.fields.file["en-US"].url
+                return <img alt={alt} src={url} />
+            },
+        },
+    }
 
+    return (
+        <Layout>
+            <SEO
+                title={props.data.contentfulBlog.title}
+                description="Insper Mileage post"
+                lang="pt-br"
+                meta={[`Insper`, `post`, `Insper Mileage`]}
+            />
+            <section class="hero is-medium">
+                <div class="hero-body">
+                    <div class="post">
+                        <h1 class="title">{props.data.contentfulBlog.title}</h1>
+                        <h2 class="subtitle">
+                            {props.data.contentfulBlog.date}
+                        </h2>
+                        <img src={props.data.contentfulBlog.image.file.url} />
 
-  return (
-    <Layout>
-      <SEO
-            title={props.data.contentfulBlog.title}
-            description="Insper Mileage post"
-            lang="pt-br"
-            meta={[
-                `Insper`,
-                `post`,
-                `Insper Mileage`,
-            ]}
-        />
-    <section class="hero is-medium">
-      <div class="hero-body">
-        <div class="post">
-          <h1 class="title">{props.data.contentfulBlog.title}</h1>
-          <h2 class="subtitle">{props.data.contentfulBlog.date}</h2>
-            <img src ={props.data.contentfulBlog.image.file.url} />
-            
-          
-          {documentToReactComponents(props.data.contentfulBlog.description.json,options)}
-        </div>
-      </div>
-    </section>
-    </Layout>
-  )
+                        {documentToReactComponents(
+                            props.data.contentfulBlog.description.json,
+                            options
+                        )}
+                    </div>
+                </div>
+            </section>
+        </Layout>
+    )
 }
 
 export default Blog
